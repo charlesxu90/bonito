9425e46901a692bf31dd7f4096b3d6858a25133e
Xiaopeng Xu <charlesxu90@gmail.com>
2023-09-24 11:23:14 +0300

    add running script

---
+++ run.sh
@@ -0,0 +1,32 @@
+# Interface
+# bonito view - view a model architecture for a given .toml file and the number of parameters in the network.
+# bonito train - train a bonito model.
+# bonito evaluate - evaluate a model performance.
+# bonito download - download pretrained models and training datasets.
+# bonito basecaller - basecaller (.fast5 -> .bam).
+
+# Download data
+# bonito download --training
+# Save to path /home/xiaopeng/miniconda3/envs/nanokws-env/lib/python3.8/site-packages/bonito/data/ 
+
+# Download models
+# bonito download --models --show  # show all available models
+# bonito download --models         # download all available models
+# Save to path /home/xiaopeng/miniconda3/envs/nanokws-env/lib/python3.8/site-packages/bonito/models/
+
+# Train models from scratch
+# bonito train --directory /data/training/ctc-data /data/training/model-dir
+bonito train --directory data/data/dna_r9.4.1/ --config data/configs/dna_r9.4.1@v3.1.toml results/model-dir
+
+# Fine-tune models
+# bonito train --epochs 1 --lr 5e-4 --pretrained dna_r10.4.1_e8.2_400bps_hac@v4.0.0 --directory /data/training/ctc-data /data/training/fine-tuned-model
+
+# DNA basecaller
+# bonito basecaller dna_r9.4.1 --save-ctc --reference reference.mmi /data/reads > /data/training/ctc-data/basecalls.sam
+# bonito basecaller dna_r10.4.1_e8.2_400bps_hac@v4.0.0 /data/reads > basecalls.bam
+
+# DNA basecaller with reference
+# bonito basecaller dna_r10.4.1_e8.2_400bps_hac@v4.0.0 --reference reference.mmi /data/reads > basecalls.bam
+
+# DNA basecaller with reference and modified base calling
+# bonito basecaller dna_r10.4.1_e8.2_400bps_hac@v4.0.0 /data/reads --modified-bases 5mC --reference ref.mmi > basecalls_with_mods.bam
