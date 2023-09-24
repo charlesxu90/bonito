48696b4123c76220c7ede270e713840d1cb08e7c
Xiaopeng Xu <charlesxu90@gmail.com>
2023-09-24 23:00:45 +0300

    test train

--- run.sh
+++ run.sh
@@ -16,14 +16,15 @@
 
 # Train models from scratch
 # bonito train --directory /data/training/ctc-data /data/training/model-dir
-bonito train --directory data/data/dna_r9.4.1/ --config data/configs/dna_r9.4.1@v3.1.toml results/model-dir
+# bonito train --directory data/data/dna_r9.4.1/ --config data/configs/dna_r9.4.1@v3.1.toml results/model-dir
+# bonito evaluate --directory data/data/dna_r9.4.1/ results/model-dir
 
 # Fine-tune models
 # bonito train --epochs 1 --lr 5e-4 --pretrained dna_r10.4.1_e8.2_400bps_hac@v4.0.0 --directory /data/training/ctc-data /data/training/fine-tuned-model
 
 # DNA basecaller
 # bonito basecaller dna_r9.4.1 --save-ctc --reference reference.mmi /data/reads > /data/training/ctc-data/basecalls.sam
-# bonito basecaller dna_r10.4.1_e8.2_400bps_hac@v4.0.0 /data/reads > basecalls.bam
+bonito basecaller dna_r10.4.1_e8.2_400bps_hac@v4.0.0 /mnt/data/data_repository/nanopore/RNA/Native_RNA/Fast5/Bham_Run1_20171009_DirectRNA_Multi_fast5 > basecalls.bam
 
 # DNA basecaller with reference
 # bonito basecaller dna_r10.4.1_e8.2_400bps_hac@v4.0.0 --reference reference.mmi /data/reads > basecalls.bam
